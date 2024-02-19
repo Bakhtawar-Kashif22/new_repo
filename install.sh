aaedea85dcd2a4cc5a770e49128da352f09bf2b7
Bakhtawar-Kashif22 <bakhtawar.kashif21@gmail.com>
2024-02-19 08:23:42 +0000

    Installation of dotfiles has completed!

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+
+#3ln -s /dotfiles/vimrc  /Downloads/Vimrc
+ln -s /dotfiles/.vimrc ~/Vimrc 
+#ln -s /dotfiles/.bashrc ~/Bashrc
+#ln -s /dotfiles/.tmux.conf ~/Tmux_conf
+
+#I can later on add more short cuts of my configuration files to use them remotely and locally
+echo "Installation of dotfiles has been completed."
